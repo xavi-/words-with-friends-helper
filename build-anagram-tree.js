19207b96f13f8444dfa438d4248f18210b57517e
Xavi <xavi.rmz@gmail.com>
2012-11-20 19:40:34 -0800

    Removed some white space.  Also renamed variable.

--- build-anagram-tree.js
+++ build-anagram-tree.js
@@ -1,18 +1,16 @@
 var fs = require("fs");
 var repl = require("repl");
-
 var _ = require("lodash");
 
 var dict = JSON.parse(fs.readFileSync("./dictionary.json"));
-var anagram = {};
-
 
 console.time("build anagram lookup");
+var lookup = {};
 dict.forEach(function(word) {
 	var normalized = _.sortBy(word).join("");
 
-	anagram[normalized] = anagram[normalized] || [];
-	anagram[normalized].push(word);
+	lookup[normalized] = lookup[normalized] || [];
+	lookup[normalized].push(word);
 });
 console.timeEnd("build anagram lookup");
 
@@ -24,4 +22,4 @@ var r = repl.start({
 	useColor: true
 });
 r.context.dict = dict;
-r.context.anagram = anagram;
+r.context.lookup = lookup;
