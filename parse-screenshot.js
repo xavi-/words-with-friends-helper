0c67488b1af5a1488c9427a7c49e9a21cc4f0ad7
Xavi <xavi.rmz@gmail.com>
2012-11-25 12:58:28 -0800

    Use dot instead of blank space to represent false value when printing out snip.

--- parse-screenshot.js
+++ parse-screenshot.js
@@ -22,7 +22,7 @@ function printSnip(snip) {
 	for(var r = 0, i = 0; r < 41; r++) {
 		var tmp = [];
 		for(var c = 0; c < 41; c++) {
-			tmp.push(snip[i++] ? "#" : " ");
+			tmp.push(snip[i++] ? "#" : ".");
 		}
 		out.push(tmp.join(""));
 	}
